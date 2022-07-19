35246435c3867714bcf6227dcf4d2543c8a0447a
Lifulifu <andycscsmaple@gmail.com>
2022-07-20 00:38:15 +0800

    init

---
+++ src/components/NotesSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+
+export default function NotesSection() {
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant='h4'>
+          myass
+        </Typography>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio consequuntur quisquam voluptates, numquam aut dolorem soluta facilis culpa? Praesentium perferendis labore autem quisquam? Accusantium quaerat suscipit eligendi deleniti porro excepturi ratione impedit vel hic commodi, eaque reiciendis magni? Alias aspernatur quisquam exercitationem sit omnis odit numquam praesentium velit laborum nemo?
+      </CardContent>
+    </Card>
+  )
+}
